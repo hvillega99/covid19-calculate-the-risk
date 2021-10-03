8c52f873355c1fa7b23fce8a044814e6735324f9
jbpita <jbpita@espol.edu.ec>
2021-10-03 09:54:06 -0500

    actualización

--- covidApp/App.js
+++ covidApp/App.js
@@ -4,10 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 import Body from './components/landingPage/Body';
 import ScreenNews from './components/news/screenNews';
 import BodyPrecauciones from './components/recomendacionesPage/body'
-//import ScreenStatistics from './components/statistics/screenStatistics';
+import ScreenStatistics from './components/statistics/screenStatistics';
+//import { NavigationContainer } from '@react-navigation/native';import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
 export default function App() {
   return (
-    <BodyPrecauciones/> 
+    <ScreenStatistics/> 
   );
   }
 
